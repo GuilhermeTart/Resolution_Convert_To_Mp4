b29302005935b1c6984ca24cfd780bdb4fa90bdd
GuilhermeTart <corp.tart@gmail.com>
2023-10-18 12:47:34 -0300

    Convet_colors

--- Convert.py
+++ Convert.py
@@ -1,9 +1,40 @@
 import subprocess
+from colors import colors
 
-path_movie = input('Insert the path video: ')
-resolution = input('Insert the resolution: ')
+print(f"""{colors.red}
+          
 
 
-Command = f"ffmpeg -i {path_movie} -vf scale={resolution} Movie_convert.mp4"
+   ______                           __     __                             __ __
+  / ____/___  ____ _   _____  _____/ /_   / /_____       ____ ___  ____  / // /
+ / /   / __ \/ __ \ | / / _ \/ ___/ __/  / __/ __ \     / __ `__ \/ __ \/ // /_
+/ /___/ /_/ / / / / |/ /  __/ /  / /_   / /_/ /_/ /    / / / / / / /_/ /__  __/
+\____/\____/_/ /_/|___/\___/_/   \__/   \__/\____/    /_/ /_/ /_/ .___/  /_/   
+                                                               /_/             
 
-subprocess.run(Command,shell=True)
+
+        
+        
+        
+        
+ {colors.reset}""")
+
+
+
+
+
+
+
+path_movie = input(f'{colors.green}Insert the path video: {colors.reset} ')
+resolution = input(f'{colors.green}Insert the resolution: {colors.reset} ')
+name_movie = input(f'{colors.green}Enter the name of video:  {colors.reset}')
+
+
+
+def convert_to_mp4():
+    Command = f"ffmpeg -i {path_movie} -vf scale={resolution} {name_movie}"
+
+    subprocess.run(Command,shell=True)
+
+
+convert_to_mp4()

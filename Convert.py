9a3bb2fa65b5f1ed15b5c670eb58e3bc957a76fc
GuilhermeTart <corp.tart@gmail.com>
2023-10-19 12:44:40 -0300

    Modify

--- Convert.py
+++ Convert.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 from colors import colors
 
 print(f"""{colors.red}
@@ -23,18 +24,32 @@ print(f"""{colors.red}
 
 
 
-
-
-path_movie = input(f'{colors.green}Insert the path video: {colors.reset} ')
-resolution = input(f'{colors.green}Insert the resolution: {colors.reset} ')
-name_movie = input(f'{colors.green}Enter the name of video:  {colors.reset}')
-
-
-
 def convert_to_mp4():
+
+    path_movie = input(f'{colors.green}Insert the path video: {colors.reset} ')
+    resolution = input(f'{colors.green}Insert the resolution: {colors.reset} ')
+    name_movie = input(f'{colors.green}Enter the name of video:  {colors.reset}')
+
+
+    if not path_movie or not resolution or not name_movie:
+        print('Please provide the correct data')
+        return
+
+
+
+    if not os.path.isfile(path_movie):
+        print(f"Files {path_movie} does not exist.")
+        return
+    
+ 
+
+    if not resolution.isdigit() or int (resolution) <= 0:
+        print("Invalid Resolution")
+        return
+    
+    
     Command = f"ffmpeg -i {path_movie} -vf scale={resolution} {name_movie}"
 
     subprocess.run(Command,shell=True)
-
-
+    
 convert_to_mp4()
